79fb3f06ac859cf12f2e2780f386f655964a66b9
Thomas <fangjingming0330@gmail.com>
2025-11-04 14:57:10 -0800

    wordle bot progress

--- Assets/Scripts/Bot/WordleBot.cs
+++ Assets/Scripts/Bot/WordleBot.cs
@@ -3,15 +3,51 @@ using UnityEngine;
 
 public class WordleBot
 {
-    private List<string> possibleWords;
+    private List<string> remainingPossibleWords;
+    private Dictionary<string, float> wordEntropies;
+
+    private WordList wordList;
+    
 
     public WordleBot(List<string> possibleWords)
     {
-        this.possibleWords = possibleWords;
+        this.remainingPossibleWords = possibleWords;
     }
 
     public string GetBestGuess()
     {
-        return "";
+        string bestGuess = "";
+        float maxEntropy = 0;
+
+        // For each valid guess, calculate expected information gain
+        foreach (string guess in remainingPossibleWords)
+        {
+            float entropy = CalculateExpectedEntropy(guess);
+            if (entropy > maxEntropy)
+            {
+                maxEntropy = entropy;
+                bestGuess = guess;
+            }
+        }
+
+        return bestGuess;
+    }
+
+    private float CalculateExpectedEntropy(string guess)
+    {
+        Dictionary<int, List<string>> patternDistribution = PatternMatcher.GetPatternDistribution(guess, remainingPossibleWords);
+
+        return 0;
+    }
+    
+    public void ProcessFeedback(string guess, GuessResult result)
+    {
+        // Filter remaining words based on pattern
+        remainingPossibleWords = PatternMatcher.FilterByPattern(
+            remainingPossibleWords, guess, result
+        );
+        
+        // Clear entropy cache since possibilities changed
+        wordEntropies.Clear();
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,13 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private string answer;
 
+    private WordleBot wordleBot;
+
     void Start()
     {
         answer = WordList.Instance.GetRandomAnswer();
+
+        wordleBot = new WordleBot(WordList.Instance.GetAllValidWords());
 
     }
 
